d261ea1df0f3f1281eadd36c37a4eb41ca9fc76e
Jonathan R <jreyes.satx@gmail.com>
2015-11-04 13:30:08 -0600

    saved changes before next exercise

--- highlow.php
+++ highlow.php
@@ -1,13 +1,25 @@
 <?php
-    $random = rand ( 1 , 100 );
+    echo "We gunna play a game now, you see?\nYou going to guess a number 'tween 1 and-uh hundred\n";
+    echo 'I\'m going to give you clues if ya don\'ts guess right. You & I\'s we\'re guuna have some fun' . "\n";
+
+    $random = mt_rand ( 1 , 100 );
     echo "$random\n";
-    echo "We gunna play a game now, you see?\nYou are going to guess a number 'tween 1 & 100\n";
-    echo 'I\'m going to give you clues if ya don\'ts guess it right. You & I\'s we\'re guuna have some fun' . "\n";
-    fwrite(STDOUT, 'Guess, I said! ');
-    $guess = fgets(STDIN);
-    if ($random != $guess) {
-        fwrite(STDOUT, "Your guess {$guess} was w-r-o-n-g. \nGuess again ");
-        $guess = fgets(STDIN);
-    }
+
+    do {
+
+        fwrite(STDOUT, 'Guess, I said! ');
+        $guess = trim(fgets(STDIN));
+        
+        if ($random > $guess) {
+            fwrite(STDOUT, "Your guess {$guess} was too LOW.\n ");
+        } else if ($random < $guess) {
+            fwrite(STDOUT, "Your guess {$guess} was too HIGH.\n");
+        } else if ($random == $guess) {
+            fwrite(STDOUT, "{$guess}, you are CORRECT\n");
+        }
+
+    } while ($guess != $random);
+    
+
 
 ?>
